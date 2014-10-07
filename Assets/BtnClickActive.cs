2c194be7e35a5a8904d74e122e2304d83f8c163a
Ash Blue <ash@blueashes.com>
2014-10-06 17:50:13 -0700

    Start of interactive scripts

--- Assets/BtnClickActive.cs
+++ Assets/BtnClickActive.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class BtnClickActive : MonoBehaviour {
+	[SerializeField] Color colorActive;
+	Color colorDefault;
+	Button[] buttons;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		buttons = GetComponentsInChildren<Button>();
+		for (int i = 0, l = buttons.Length; i < l; i++) {
+			Button btn = buttons[i];
+			buttons[i].onClick.AddListener(() => { SetActive(btn); });
+		}
+
+		colorDefault = buttons[0].image.color;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	}
+
+	void SetActive (Button btn) {
+		for (int i = 0, l = buttons.Length; i < l; i++) {
+			buttons[i].image.color = colorDefault;
+		}
+
+		btn.image.color = colorActive;
 	}
 }
---
+++ Assets/InventoryContentManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InventoryContentManager : MonoBehaviour {
+	List<Transform> children;
+
+	void Awake () {
+
+		children = new List<Transform>();
+		foreach (Transform child in transform) {
+			children.Add(child);
+		}
+	}
+
+	public void HideAll () {
+		foreach (Transform trans in children) {
+			trans.gameObject.SetActive(false);
+		}
+	}
+}
