1a52aea9d7e38824083105780ae970b1f10b15a5
Ash Blue <ash@blueashes.com>
2014-10-07 17:31:10 -0700

    Added data mock for JSON items

---
+++ Assets/LoadInventory.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Loader {
+	public class LoadInventory : MonoBehaviour {		
+		void Awake () {
+			string dataMock = Resources.Load("Data/Items").ToString();
+			JSONObject data = new JSONObject(dataMock);
+			foreach(JSONObject j in data.list) {
+				// Generate prefab
+				for (int i = 0; i < j.list.Count; i++){
+					string key = (string)j.keys[i];
+					JSONObject val = (JSONObject)j.list[i];
+					Debug.Log(key);
+					Debug.Log(val.str);
+					// Update prefab values
+				}
+			}
+		}
+	}
+}
+
